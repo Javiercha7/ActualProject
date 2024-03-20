4db48dadc1c8aedd11167d92438aacedd7542dbc
Javiercha7 <114618559+Javiercha7@users.noreply.github.com>
2024-03-20 18:03:37 -0400

    Update CatMovement.cs
    respawn and possibly checkpoint

--- CatMovement.cs
+++ CatMovement.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class CatMovement : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
+    private Vector3 respawnPoint;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        respawnPoint = transform.position;
     }
 
     // Update is called once per frame
@@ -18,6 +19,17 @@ public class CatMovement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) == true)
         {
             myRigidbody.velocity = Vector2.up * 8;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collison.tag == "FallDetector")
+        {
+            transform.position.position = respawnPoint;
+        }
+        else if (collision.tag == "Checkpoint")
+        {
+            respawnPoint = transform.position;
         }
     }
 }
