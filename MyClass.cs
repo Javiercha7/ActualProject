12833a9fdbe75bf9440240949ae5ed64007445e7
Javiercha7 <114618559+Javiercha7@users.noreply.github.com>
2024-03-20 16:41:44 -0400

    Update Req

---
+++ MyClass.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public static Vector3 CurrentRespawnPoint;
+
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.CompareTag("Cat"))
+        {
+            CurrentRespawnPoint = transform.position;
+        }
+    }
+}
