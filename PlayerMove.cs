fe1c19c0e1513a9421adde85c5214524ff0906c1
Javiercha7 <114618559+Javiercha7@users.noreply.github.com>
2024-04-03 18:07:02 -0400

    Add files via upload
    The code that actually lets the player move around and jump

---
+++ PlayerMove.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed;
+    public float jump;
+
+    private float move;
+
+    public Rigidbody2D rb;
+
+    public bool isJumping;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(speed * move, rb.velocity.y);
+
+        if(Input.GetButtonDown("Jump") && isJumping == false) //If the space key is clicked the player would start to jump
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, jump));
+        }
+    }
+
+
+
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Floor"))
+        {
+            isJumping = false;
+        }
+
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            isJumping = true;
+        }
+
+    }
+
+}
+
